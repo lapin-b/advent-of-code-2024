f7fb65a7bcf10db9217275e22219d5d13b1dc487
l4p1n <l4p1n@l4p1n.ch>
2024-12-03 16:18:01 +0100

    Add day 3 part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Day3;
+
+public partial class Program
+{
+    public static void Main()
+    {
+        var content = File.ReadAllText("files/input.txt").Trim();
+        Part1(content);
+    }
+
+    private static void Part1(string content)
+    {
+        var mulInstructions = MulInstructionFinder().Matches(content);
+        var result = mulInstructions
+            .Select(match =>
+            {
+                Console.WriteLine($"Found {match.Value}");
+                return (int.Parse(match.Groups["t1"].Value), int.Parse(match.Groups["t2"].Value));
+            })
+            .Sum(item => item.Item1 * item.Item2);
+        
+        Console.WriteLine("Part 1: " + result);
+    }
+
+    [GeneratedRegex(@"mul\((?<t1>\d+),(?<t2>\d+)\)")]
+    private static partial Regex MulInstructionFinder();
+}
