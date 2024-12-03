1fd4e76a94fb8ad511e9625d3d8bb27ddf160dc3
l4p1n <l4p1n@l4p1n.ch>
2024-12-03 16:49:31 +0100

    Add day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -7,8 +7,9 @@ public partial class Program
 {
     public static void Main()
     {
-        var content = File.ReadAllText("files/input.txt").Trim();
+        var content = File.ReadAllText("files/input.txt").Replace("\n", "").Trim();
         Part1(content);
+        Part2(content);
     }
 
     private static void Part1(string content)
@@ -17,7 +18,7 @@ public partial class Program
         var result = mulInstructions
             .Select(match =>
             {
-                Console.WriteLine($"Found {match.Value}");
+                Console.WriteLine($"Found: {match.Value}");
                 return (int.Parse(match.Groups["t1"].Value), int.Parse(match.Groups["t2"].Value));
             })
             .Sum(item => item.Item1 * item.Item2);
@@ -25,6 +26,40 @@ public partial class Program
         Console.WriteLine("Part 1: " + result);
     }
 
+    private static void Part2(string content)
+    {
+        var instructions = ConditionalMulInstructionFinder().Matches(content);
+        var result = instructions.Aggregate(new { Acc = 0, MulEnabled = true }, (state, instruction) =>
+        {
+            Console.WriteLine($"Found {instruction.Value}");
+            
+            if (instruction.Value.Equals("do()"))
+            {
+                return state with { MulEnabled = true };
+            }
+
+            if (instruction.Value.Equals("don't()"))
+            {
+                return state with { MulEnabled = false };
+            }
+
+            if (!instruction.Value.StartsWith("mul(")) throw new ArgumentOutOfRangeException(nameof(instruction), instruction.Value);
+            
+            if (!state.MulEnabled)
+            {
+                return state;
+            }
+                
+            var (t1, t2) = (int.Parse(instruction.Groups["t1"].Value), int.Parse(instruction.Groups["t2"].Value));
+            return state with { Acc = state.Acc + t1 * t2 };
+        });
+
+        Console.WriteLine("Part 2: " + result.Acc);
+    }
+
     [GeneratedRegex(@"mul\((?<t1>\d+),(?<t2>\d+)\)")]
     private static partial Regex MulInstructionFinder();
+    
+    [GeneratedRegex(@"do(?:n't)?\(\)|mul\((?<t1>\d+),(?<t2>\d+)\)")]
+    private static partial Regex ConditionalMulInstructionFinder();
 }
