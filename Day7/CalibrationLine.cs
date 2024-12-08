57f75a5fcdae686a09ec1311828e6e75c4dee43e
l4p1n <l4p1n@l4p1n.ch>
2024-12-08 18:48:54 +0100

    Add day 7 part 1

---
+++ Day7/CalibrationLine.cs
@@ -0,0 +1,116 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Day7;
+
+public class CalibrationLine
+{
+    private ulong _target;
+    private ulong[] _operands;
+    private Operator[] _operators = [];
+    private string _originalString;
+    public SolvedState IsSolved { get; private set; } = SolvedState.NotTried;
+
+    private static Regex _extractionRegex = new(@"(?<Result>\d+): (?:(?<Operand>\d+) ?)+");
+    
+    public CalibrationLine(string line)
+    {
+        var components = _extractionRegex.Match(line.Trim());
+        _operands = components.Groups["Operand"].Captures.Select(c => ulong.Parse(c.Value)).ToArray();
+        _target = ulong.Parse(components.Groups["Result"].Value);
+        _originalString = line;
+    }
+
+    public void SolveEquation()
+    {
+        SolveEquationRecursive([], _operands[0], 1);
+        
+        // Perform sanity checks
+        if (IsSolved == SolvedState.Solved)
+        {
+            if (_operators.Length >= _operands.Length)
+            {
+                throw new Exception($"Operators count > operands count: {_operators.Length} >= {_operands.Length}");
+            }
+
+            var actualCalculation = ExecuteCalculation();
+            if (actualCalculation != _target)
+            {
+                throw new Exception($"Equality is not upheld (target: {_target}, actual: {actualCalculation})");
+            }
+        }
+
+        // Still marked as not tried ? Must be impossible to solve then.
+        if (IsSolved == SolvedState.NotTried)
+        {
+            IsSolved = SolvedState.ImpossibleToSolve;
+        }
+    }
+
+    public ulong ExecuteCalculation()
+    {
+        return _operands[1..]
+            .Zip(_operators)
+            .Aggregate(_operands[0], (acc, current) => ExecuteOperator(current.Second, acc, current.First));
+    }
+
+    public override string ToString() => _originalString;
+
+    private void SolveEquationRecursive(List<Operator> operators, ulong accumulator, int currentOperandPosition)
+    {
+        // End conditions
+        // 1. Accumulator is equal to the target
+        if (accumulator == _target)
+        {
+            _operators = operators.ToArray();
+            IsSolved = SolvedState.Solved;
+            return;
+        }
+        
+        // 2. Ran out of operands
+        // 3. Operators array is already filled
+        if (currentOperandPosition > _operands.Length - 1 || _operators.Length > 0)
+        {
+            return;
+        }
+
+        var currentOperand = _operands[currentOperandPosition];
+        
+        // Step
+        foreach (var op in new[] { Operator.Add, Operator.Mul })
+        {
+            operators.Add(op);
+            SolveEquationRecursive(
+                operators, 
+                ExecuteOperator(op, accumulator, currentOperand),
+                currentOperandPosition + 1
+            );
+            operators.RemoveAt(operators.Count - 1);
+
+            if (IsSolved == SolvedState.Solved)
+            {
+                break;
+            }
+        }
+    }
+
+    private ulong ExecuteOperator(Operator op, ulong op1, ulong op2) => op switch
+    {
+        Operator.Mul => op1 * op2,
+        Operator.Add => op1 + op2,
+        _ => throw new ArgumentOutOfRangeException(nameof(op), op, null)
+    };
+}
+
+public enum Operator
+{
+    Mul,
+    Add,
+}
+
+public enum SolvedState
+{
+    Solved,
+    NotTried,
+    ImpossibleToSolve,
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fileContent = File.ReadAllLines("files/input.txt");
+        var equations = fileContent.Select(l => new CalibrationLine(l)).ToArray();
+
+        var chronoStart = DateTime.Now;
+        foreach (var equation in equations)
+        {
+            Console.WriteLine(equation);
+            equation.SolveEquation();
+        }
+
+        var chronoFinish = DateTime.Now;
+
+        Console.WriteLine($"Solved {equations.Length} in {(chronoFinish - chronoStart).TotalSeconds} seconds");
+        
+        // Part 1
+        var solvedEquationsTotal = equations
+            .Where(e => e.IsSolved == SolvedState.Solved)
+            .Aggregate((ulong)0, (acc, current) => acc + current.ExecuteCalculation());
+
+        Console.WriteLine($"Part 1: Sum of solved equations: {solvedEquationsTotal}");
+    }
+}
